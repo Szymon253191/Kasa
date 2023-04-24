6e19eab44f3da32a1b84dfc6696cc9f0c213f5ad
Szymon <62205129+Szymon253191@users.noreply.github.com>
2023-04-24 21:22:20 +0200

    sync

--- KasaLibrary/DataAccess/JsonConnector.cs
+++ KasaLibrary/DataAccess/JsonConnector.cs
@@ -20,6 +20,8 @@ namespace CashLibrary.DataAccess
                 {
                     string json = reader.ReadToEnd();
                     products = JsonConvert.DeserializeObject<List<ProductModel>>(json);
+                    if (!ValidateData(products))
+                        new Exception("Bład w spójności danych");
                     return products;
                 }
             }
@@ -28,6 +30,35 @@ namespace CashLibrary.DataAccess
                 Console.WriteLine("Błąd w pobieraniu danych");
                 throw;
             }
+        }
+
+        public static bool ValidateData(List<ProductModel> products)
+        {
+            bool flag = true;
+            List<int> Ids = new List<int>();
+
+            foreach (ProductModel product in products)
+            {
+                if (product.Name == "" || product.Name == null)
+                {
+                    flag = false;
+                    throw new Exception($"Nazwa jednego z produktów jest błędnie zapisana w bazie: {product.Id + " " + product.Name + " " + product.Price}");
+                }
+                else if (product.Price == null || product.Price < 0f)
+                {
+                    flag = false;
+                    throw new Exception($"Cena jednego z produktów jest błędnie zapisana w bazie: {product.Id + " " + product.Name + " " + product.Price}");
+                }
+                else if (product.Id == null || Ids.Contains(product.Id))
+                {
+                    flag = false;
+                    throw new Exception($"Numery ID produktu w bazie nie są unikalne lub nie istnieją: {product.Id + " " + product.Name + " " + product.Price}");
+                }
+
+                Ids.Add(product.Id);
+            }
+
+            return flag;
         }
     }
 }
